f3b6ed60eb4ea61debc5288bbaea5fe7f3bd5685
Zachary Ross <zmanross44@gmail.com>
2022-12-20 13:27:15 -0500

    Cleaning out project files

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,15 +1,20 @@
-import './App.scss';
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<Layout />}></Route>
-      </Routes>
-    </>
-  )
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
 }
-
-export default App
